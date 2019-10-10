6e50d563445238d330e67d0509aa2bec50207172
Sergio Martinez Roman <smartinezr@6TT7952-PC.carver-as.com>
2019-10-10 09:56:52 +0200

    Revert "Update bootstrap-treenode.js"
    This reverts commit ba273ebd8dfc34dbd7aabe8c63b906eae6500181.

--- WebContent/bootstrap-treenode.js
+++ WebContent/bootstrap-treenode.js
@@ -1,7 +1,8 @@
 /* =========================================================
  * bootstrap-treeview.js v1.2.0
  * =========================================================
- * Copyright 2019 Sergio Martínez
+ * Modifications 2019 Sergio Martínez
+ * Copyright 2013 Jonathan Miles
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
